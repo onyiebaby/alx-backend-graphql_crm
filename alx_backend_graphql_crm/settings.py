d437b5b1615e6f290191bc749e3d2e4b536146b9
Onyiebaby <onyinyechimatthew06@gmail.com>
2025-10-27 19:43:48 +0100

    Add schema.py

--- alx_backend_graphql_crm/settings.py
+++ alx_backend_graphql_crm/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'alx_backend_graphql_crm.crm'
+    'alx_backend_graphql_crm.crm',
+    'crm',
+    'graphene-django',
 ]
 
 MIDDLEWARE = [
---
+++ schema.py
@@ -0,0 +1,16 @@
+import graphene
+from graphene_django import DjangoObjectType
+from alx-backend-graphql_crm.crm.models import Category, crm
+
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = Category
+        fields = ("id", "name", "email")
+
+class Query(graphene.ObjectType):
+    all_customers = graphene.List(CustomerType)
+
+    def resolve_all_customers(root, info):
+        return Customer.objects.all()
+    
+schema = graphene.Schema(query=Query)    
