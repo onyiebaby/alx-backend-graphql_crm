6266d2543b04041bac03a58bcdfc3fbb9b5e4ea1
Onyiebaby <onyinyechimatthew06@gmail.com>
2025-10-28 12:36:46 +0100

    Add GraphQL

--- crm/models.py
+++ crm/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model
+               
+class Customer(models.Model):
+
+models.CharField(max_length=100)", "name"
+
+email: models.EmailField()
+
+"def __str__(self):", "self.name"
